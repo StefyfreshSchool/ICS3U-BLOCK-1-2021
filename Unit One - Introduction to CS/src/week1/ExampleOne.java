0d9e7fb5ced0cf7bb2d9b40b5bf616b8186e2242
Kevin DesLauriers <kevindeslauriers@gmail.com>
2021-09-09 09:24:11 -0400

    Initial Commit for Unit One

---
+++ Unit One - Introduction to CS/src/week1/ExampleOne.java
@@ -0,0 +1,16 @@
+package week1;
+
+// Every file will be its own class FOR NOW
+public class ExampleOne {
+
+  public static void main(String[] args) {
+    // The main method (function) let's Java know ehere to start.
+
+    // System.out.println writes to the console.
+    // Whatever is in the round brackets goes in the console.
+    System.out.println("Welcome to ICS 3U - AP");
+    System.out.println("Welcome to ICS 3U - AP");
+    System.out.println("Welcome to ICS 3U - AP");
+    System.out.println("Welcome to ICS 3U - AP");
+  }
+}
