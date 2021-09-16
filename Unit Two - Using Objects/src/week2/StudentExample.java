7a38a52935fa30036d666152242365f02571198f
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-16 09:51:49 -0400

    Using non-static void object methods with and without parameters

--- Unit Two - Using Objects/src/week2/StudentExample.java
+++ Unit Two - Using Objects/src/week2/StudentExample.java
@@ -7,5 +7,35 @@ public class StudentExample {
     shohei = new Student("Shohei", "123456", 11);
     // shohei is a Student (object variable)
 
+    Student samantha = new Student("Samantha", "654321", 11);
+
+    Student alan = shohei;
+
+    // called (activated) the method increaseGrade
+    // because alan and shohei reference the same Student Object both of their
+    // grades increase (there is only 1 object)
+    alan.increaseGrade();
+
+    // we activate methods(functions) through the object (variable name)
+    samantha.displayName();
+    samantha.displayStudentNumber();
+    samantha.increaseGrade();
+
+    samantha = null;
+    alan = new Student("Alan", "555555", 11);
+    // samantha.displayGrade(); // null pointer exception cannot call a method from
+    // a null reference
+
+    alan.addTest(87);
+    alan.displayAverage();
+    alan.addTest(92);
+    alan.displayAverage();
+    alan.addTest(96);
+    alan.displayAverage();
+    alan.addTest(67);
+    alan.displayAverage();
+
+    // alan.numMarks = 7; don't have access to primate members (not visible)
+
   }
 }
