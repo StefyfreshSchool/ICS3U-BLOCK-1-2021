f5be1f94bc694c997b24b6b7a90637ee6dbb4341
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-28 12:28:30 -0400

    Formatted BVG Pools

--- Unit Two - Using Objects/src/week4/BayviewGlenPools.java
+++ Unit Two - Using Objects/src/week4/BayviewGlenPools.java
@@ -14,8 +14,9 @@ public class BayviewGlenPools {
             double surfaceArea = getSurfaceArea(length, width, shallowHeight, shallowLength, transition, deepHeight);
             double volume = getVolume(length, width, shallowHeight, shallowLength, transition, deepHeight);
 
-            System.out.println(volume);
-            System.out.println(surfaceArea);
+            System.out.println("Volume: " + (int) (volume * 0.9 * 100) / 100.0);
+            System.out.println("Surface Area: " + (int) (surfaceArea * 100) / 100.0);
+            System.out.println("Cost of Liner: $" + (int) ((surfaceArea * linerCost) * 100) / 100.0);
 
       }
 
