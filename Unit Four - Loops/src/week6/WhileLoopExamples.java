f097b35939d6ab024bfeb69c50d297c5ee21bd23
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-10-08 09:26:18 -0400

    ValidInput

--- Unit Four - Loops/src/week6/WhileLoopExamples.java
+++ Unit Four - Loops/src/week6/WhileLoopExamples.java
@@ -1,5 +1,7 @@
 package week6;
 
+import java.util.Scanner;
+
 public class WhileLoopExamples {
    public static void main(String[] args) {
       exampleOne();
@@ -8,6 +10,13 @@ public class WhileLoopExamples {
       int numVowels = countVowels("This is a sentence with VOWELS!");
       System.out.println(numVowels);
 
+      Scanner in = new Scanner(System.in);
+      int num = getValidInput(10, 20);
+
+   }
+
+   private static int getValidInput(int i, int j) {
+      return 0;
    }
 
    private static int getSumInclusive(int i, int j) {
