955c126a8d7b09c1686ca373e9400b08785f3197
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-10-20 08:21:41 -0400

    Discard Function

--- Unit Four - Loops/src/week7/ThreeCardPower.java
+++ Unit Four - Loops/src/week7/ThreeCardPower.java
@@ -34,22 +34,92 @@ public class ThreeCardPower {
       System.out.println(playerHand);
 
       playerHand = discard(in, playerHand);
+      System.out.println(playerHand);
    }
 
    private static String discard(Scanner in, String playerHand) {
-      // ask the user how many cards they would like to replace.
-      // make sure it is between 0 and 3 - valid input only
+
       int numCardsToReplace = getNumber(in, "How many cards would you like to discard: ", 0, 3);
+      String temp = playerHand;
+      if (numCardsToReplace == 0)
+         return playerHand;
+      else if (numCardsToReplace == 3) {
+         playerHand = getCard(playerHand + temp) + " ";
+         playerHand += getCard(playerHand + temp) + " ";
+         playerHand += getCard(playerHand + temp) + " ";
+      } else {
+         String cardToReplace = getCardsToDiscard(in, numCardsToReplace, playerHand);
+         int space = cardToReplace.indexOf(" ");
+         String card1 = cardToReplace.substring(0, space);
+         String card = getCard(playerHand);
+         playerHand = playerHand.replace(card1, card);
+         if (numCardsToReplace == 2) {
+            String card2 = cardToReplace.substring(space + 1);
+            card = getCard(playerHand + cardToReplace);
+            playerHand = playerHand.replace(card2, card);
+         }
+      }
+      return playerHand;
+   }
 
-      // prompt them the cards to replace (if the pick 2 the they should enter 3H 7D
-      // and if they pick 1 it should look like AD)
+   private static String getCardsToDiscard(Scanner in, int numCardsToReplace, String playerHand) {
+      final String VALID_CARDS = "AS2S3S4S5S6S7S8S9S10SJSQSKSAC2C3C4C5C6C7C8C9C10CJCQCKCAD2D3D4D5D6D7D8D9D10DJDQDKDAH2H3H4H5H6H7H8H9H10HJHQHKH";
+      boolean validInput = false;
+      String cards = "";
+      while (!validInput) {
+         System.out.println("Please enter the cards to replace: (ex. 7H KD): ");
+         cards = in.nextLine().toUpperCase();
 
-      // return the new hand
+         if ((numCardsToReplace == 1) && (playerHand.indexOf(cards) < 0))
+            System.out.println("You don't have a " + cards);
+         else if ((numCardsToReplace == 1) && (playerHand.indexOf(cards) >= 0))
+            validInput = true;
+         else if (numCardsToReplace == 2) {
+            int space = cards.indexOf(" ");
+            if (space < 0) {
+               System.out.println("You must choose two cards.");
+            } else {
+               String card1 = cards.substring(0, space);
+               String card2 = cards.substring(space + 1);
+               if (VALID_CARDS.indexOf(card1) < 0)
+                  System.out.println("Not a valid card: " + card1);
+               else if (VALID_CARDS.indexOf(card2) < 0)
+                  System.out.println("Not a valid card: " + card2);
+               if (cards.indexOf(card1) < 0)
+                  System.out.println("You don't have a " + card1);
+               else if (cards.indexOf(card2) < 0)
+                  System.out.println("You don't have a " + card2);
+               else if (card1.equals(card2))
+                  System.out.println("You cannot discard the same card.");
+               else
+                  validInput = true;
+            }
+         }
+      }
+
+      return cards;
 
    }
 
    private static int getNumber(Scanner in, String prompt, int min, int max) {
-      return 0;
+      boolean validInput = false;
+      int val = 0;
+      while (!validInput) {
+         System.out.print(prompt);
+         try {
+            val = Integer.parseInt(in.nextLine());
+
+            if (val < min || val > max) {
+               System.out.println("Invalid input!");
+            } else {
+               validInput = true;
+            }
+         } catch (Exception ex) {
+            System.out.println("Invalid input!");
+         }
+      }
+      return val;
+
    }
 
    private static String getCard(String cardsUsed) {
